bdcc81afe7722338ed7d2ca5cb8a02ee3d2f30d0
Kim YoonJung <codekyz0111@gmail.com>
2024-11-04 16:12:47 +0900

    Finished Gatsby Tutorial part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,5 +39,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `article`,
+        path: `${__dirname}/article`,
+      },
+    },
   ],
 }
